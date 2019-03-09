2cfc114e010473ccd3988c684fa7fe7f187eca4b
Sebastián <cbastian320@gmail.com>
2019-03-09 16:46:07 -0500

    Arreglo de errores con la persistencia

--- ProyectoAutomatas1.0/src/persistence/Persistence.java
+++ ProyectoAutomatas1.0/src/persistence/Persistence.java
@@ -84,7 +84,7 @@ public class Persistence {
 			transitionJSON.add("To", transitionStateToJSON);	
 			
 			transitionJSON.addProperty("Character", transitionToAdd.getCharacter());	
-			finalStateListJSON.add(transitionJSON);
+			transitionlistJSON.add(transitionJSON);
 		}
 		automatonJSON.add("Lista de transiciones", transitionlistJSON);
 		
@@ -100,72 +100,4 @@ public class Persistence {
 		file.flush();
 		file.close();
 	}
-
-//	public void leerJson(String ruta) {
-//		JsonParser parser = new JsonParser();
-//		FileReader fr;
-//		try {
-//			fr = new FileReader(ruta);
-//			JsonElement datos = parser.parse(fr);
-//			dumpJSONElement(datos);
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//	}
-//
-//	public static void dumpJSONElement(JsonElement elemento) {
-//		if (elemento.isJsonObject()) {
-//			System.out.println("Es objeto");
-//			JsonObject obj = elemento.getAsJsonObject();
-//			java.util.Set<java.util.Map.Entry<String,JsonElement>> entradas = obj.entrySet();
-//			java.util.Iterator<java.util.Map.Entry<String,JsonElement>> iter = entradas.iterator();
-//			while (iter.hasNext()) {
-//				java.util.Map.Entry<String,JsonElement> entrada = iter.next();
-//				System.out.println("Clave: " + entrada.getKey());
-//				System.out.println("Valor:");
-//				dumpJSONElement(entrada.getValue());
-//			}
-//
-//		} else if (elemento.isJsonArray()) {
-//			JsonArray array = elemento.getAsJsonArray();
-//			System.out.println("Es array. Numero de elementos: " + array.size());
-//			java.util.Iterator<JsonElement> iter = array.iterator();
-//			while (iter.hasNext()) {
-//				JsonElement entrada = iter.next();
-//				dumpJSONElement(entrada);
-//			}
-//		} else if (elemento.isJsonPrimitive()) {
-//			System.out.println("Es primitiva");
-//			JsonPrimitive valor = elemento.getAsJsonPrimitive();
-//			if (valor.isBoolean()) {
-//				System.out.println("Es booleano: " + valor.getAsBoolean());
-//			} else if (valor.isNumber()) {
-//				System.out.println("Es numero: " + valor.getAsNumber());
-//			} else if (valor.isString()) {
-//				System.out.println("Es texto: " + valor.getAsString());
-//			}
-//		} else if (elemento.isJsonNull()) {
-//			System.out.println("Es NULL");
-//		} else {
-//			System.out.println("Es otra cosa");
-//		}
-//	}
-//
-//
-//	public void escribirJson(String ruta, Automaton automaton) {
-//		Gson gson = new Gson();
-//		String jsonString = gson.toJson(automaton);
-//		System.out.println("JSON: " + jsonString);  
-//		try {
-//
-//			FileWriter file = new FileWriter(ruta);
-//			file.write(jsonString);
-//			file.flush();
-//			file.close();
-//		} catch (IOException e) {
-//			System.out.println(e.getMessage());
-//		}
-//	}
 }
